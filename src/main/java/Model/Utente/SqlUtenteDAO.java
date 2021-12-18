8f93d37f5b2dd7e3af6b241393b2fa408786449e
Biancoyork <69357318+Biancoyork@users.noreply.github.com>
2021-12-18 17:08:59 +0100

    InizioDAO - primi SQL

--- src/main/java/Model/Utente/SqlUtenteDAO.java
+++ src/main/java/Model/Utente/SqlUtenteDAO.java
@@ -10,7 +10,28 @@ import java.sql.SQLException;
 public class SqlUtenteDAO implements UtenteDAO<SQLException>{
     @Override
     public boolean createUtente(Utente utente) throws SQLException {
-        return false;
+        try(Connection con = ConPool.getConnection()){
+            try(PreparedStatement ps = con.prepareStatement("Insert into Utente(nome,cognome,email,password,universitario,ruolo) " +
+                    "as value (?,?,?,?,?,?)")){
+
+                //Inserimento utente nel db
+                ps.setString(1,utente.getNome());
+                ps.setString(2,utente.getCognome());
+                ps.setString(3,utente.getEmail());
+                ps.setString(4,utente.getPassword());
+                ps.setBoolean(5,utente.getUniversitario());
+                ps.setInt(6,utente.getRuolo());
+
+                int rows = ps.executeUpdate();
+
+                /*Fetching dell'id
+                ResultSet rs = ps.getGeneratedKeys();
+                int id = rs.next() ? rs.getInt(1): -1;
+                utente.setIdUtente(id);
+                */
+                return rows == 1;
+            }
+        }
     }
 
     @Override
