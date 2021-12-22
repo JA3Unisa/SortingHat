cedb9b6307191f25f84ebf6a8af85ca48a0f5a2e
Biancoyork <69357318+Biancoyork@users.noreply.github.com>
2021-12-22 18:35:39 +0100

    Risposta SQL

--- src/main/java/Model/Risposta/RispostaDAO.java
+++ src/main/java/Model/Risposta/RispostaDAO.java
@@ -1,4 +1,7 @@
 package Model.Risposta;
 
 public interface RispostaDAO <E extends Exception>{
+    boolean createRisposta (Risposta risposta) throws E;
+    boolean updateRisposta (Risposta risposta) throws E;
+    boolean deleteRisposta (int id) throws E;
 }
--- src/main/java/Model/Risposta/SqlRispostaDAO.java
+++ src/main/java/Model/Risposta/SqlRispostaDAO.java
@@ -1,4 +1,63 @@
 package Model.Risposta;
 
+import Model.ConPool.ConPool;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+
 public class SqlRispostaDAO implements RispostaDAO{
+    @Override
+    public boolean createRisposta(Risposta risposta) throws Exception {
+        try(Connection con = ConPool.getConnection()){
+            try(PreparedStatement ps = con.prepareStatement("Insert into Risposta(corpo,dataOra,idUtente,idDiscussione) " +
+                    "as value (?,?,?,?);")){
+
+                ps.setString(1,risposta.getCorpo());
+
+                ps.setInt(3,risposta.getIdUtente());
+                ps.setInt(3,risposta.getIdDiscussione());
+
+                int rows = ps.executeUpdate();
+
+                return rows == 1;
+            }
+        }
+    }
+
+    @Override
+    public boolean updateRisposta(Risposta risposta) throws Exception {
+        try(Connection con = ConPool.getConnection()){
+            try(PreparedStatement ps = con.prepareStatement("UPDATE Utente " +
+                    "SET corpo = ?, dataOra = ?, idUtente = ?, idDiscussione = ?" +
+                    "WHERE id = ?;")){
+
+                //Inserimento utente nel db
+                ps.setString(1,risposta.getCorpo());
+                Date date = new Date(risposta.getdataOra().getTimeInMillis());
+                ps.setDate(2,date);
+                ps.setInt(3,risposta.getIdUtente());
+                ps.setInt(4,risposta.getIdDiscussione());
+                ps.setInt(5,risposta.getIdRisposta());
+
+                int rows = ps.executeUpdate();
+
+                return rows == 1;
+            }
+        }
+    }
+
+    @Override
+    public boolean deleteRisposta(int id) throws Exception {
+        try(Connection con = ConPool.getConnection()){
+            try(PreparedStatement ps = con.prepareStatement("DELETE from Risposta where id = ?;")){
+
+                ps.setInt(1,id);
+
+                int rows = ps.executeUpdate();
+
+                return rows == 1;
+            }
+        }
+    }
 }
