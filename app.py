6243f492c2d9138bf966af19c270da8f6f6ac0ac
Mattyx00 <matteosacco00@gmail.com>
2022-01-26 16:30:56 +0100

    installed flask-cors

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ from controller import *
 from machineLearning import executeMachineLearning
 import json
 from model import saveMachineLearning
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 
 @app.route('/FIA/consiglio', methods=['POST'])
+@cross_origin(supports_credentials=True)
 def contribuisci():
     try:
         content = request.get_json(silent=True)
