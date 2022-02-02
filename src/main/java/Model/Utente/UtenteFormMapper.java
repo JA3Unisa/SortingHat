b17a8e3d163172132bf1b46ba4c83f09105322a5
zEmanu <e.digregorio5@studenti.unisa.it>
2022-02-02 20:44:43 +0100

    fix Registrazione

--- src/main/java/Model/Utente/UtenteFormMapper.java
+++ src/main/java/Model/Utente/UtenteFormMapper.java
@@ -6,17 +6,22 @@ public class UtenteFormMapper {
     public Utente map(HttpServletRequest request, Boolean update)  {
         Utente utente=new Utente();
         utente.setEmail(request.getParameter("Email"));
-        //System.out.println(cliente.getEmail()+" "+request.getParameter("Mail"));
+        System.out.println(utente.getEmail()+" "+request.getParameter("Email"));
         utente.setNome(request.getParameter("Nome"));
-        //System.out.println(cliente.getNome()+" "+request.getParameter("Nome"));
+       System.out.println(utente.getNome()+" "+request.getParameter("Nome"));
         utente.setCognome(request.getParameter("Cognome"));
         //CHECK
         String email=request.getParameter("Email");
-        if(email.contains("Studenti.unisa.it")){
+        /*if(email.contains("Studenti.unisa.it")){
             utente.setUniversitario(true);
-        }else utente.setUniversitario(false);
+        }else utente.setUniversitario(false);*/
       //  utente.setUniversitario(Boolean.valueOf(request.getParameter("univesitario")));
-        utente.setRuolo(Integer.parseInt(request.getParameter("Ruolo")));
+        //utente.setRuolo(Integer.parseInt(request.getParameter("Ruolo")));
+        String i=request.getParameter("Boolean");
+        if (i.equals("Ordinario"))
+            utente.setUniversitario(false);
+        else
+            utente.setUniversitario(true);
 
         if(update){
             System.out.println();
