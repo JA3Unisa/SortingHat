68a4d0c3a6974e6bb5c527958b1e9223c7ba309d
Mattyx00 <matteosacco00@gmail.com>
2022-02-14 21:24:19 +0100

    JavaDoc

---
+++ javaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ApplicationLogic"},{"l":"ApplicationLogic.Http"},{"l":"Storage.Categoria"},{"l":"Storage.ConPool"},{"l":"Storage.Contribuisci"},{"l":"Storage.Discussione"},{"l":"Storage.Predict"},{"l":"Storage.Risposta"},{"l":"Storage.Utente"}];updateSearchResults();
